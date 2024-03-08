95e3749c554b713f80ec72b098ebfcf8f5ef11de
adudu2019 <dhiyaulhaq2097@gmail.com>
2024-03-08 09:16:02 +0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import DetailPage from "./Pages/DetailPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/shoesinn/">
       <AuthProvider>
         <App />
       </AuthProvider>
